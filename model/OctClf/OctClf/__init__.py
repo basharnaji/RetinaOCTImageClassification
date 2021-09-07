c6330c00129f060f9d357d95911accc748034835
BasharNaji <balkuwai@gmail.com>
2021-09-06 18:39:33 -0700

    Local Flask App & Deployable AWS EBS both working

--- model/OctClf/OctClf/__init__.py
+++ model/OctClf/OctClf/__init__.py
@@ -1,12 +1,11 @@
 # flake8: noqa
-
 from flask import Flask
 
 
 def create_app():
     """Initialize the core application."""
     # Initialize Flask app with '__name__' and 'instance_relative_config=False'
-    app = Flask(__name__, instance_relative_config=False)
+    app = Flask(__name__, instance_relative_config=True)
 
     with app.app_context():
         from . import routes
