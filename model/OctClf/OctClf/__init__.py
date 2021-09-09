ab17c8f8f11c5b976121c521c485dcf4b685481a
BasharNaji <balkuwai@gmail.com>
2021-09-09 10:43:51 -0700

    App without caching of images so it refreshes properly

--- model/OctClf/OctClf/__init__.py
+++ model/OctClf/OctClf/__init__.py
@@ -6,6 +6,7 @@ def create_app():
     """Initialize the core application."""
     # Initialize Flask app with '__name__' and 'instance_relative_config=False'
     app = Flask(__name__, instance_relative_config=True)
+    app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
     with app.app_context():
         from . import routes
